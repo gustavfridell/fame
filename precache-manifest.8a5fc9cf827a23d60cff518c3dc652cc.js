d6e661338a8ffa94d80d5b662994ce077c17f90f
Gustav Fridell <gustavfridell@me.com>
2020-02-01 23:14:18 +0100

    Updates

---
+++ precache-manifest.8a5fc9cf827a23d60cff518c3dc652cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b68a77824ba6ded11467f2408e1a5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d82660d3f4cf4c7a81db",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e34448a802a90a2f9c31",
+    "url": "/static/js/2.b69789a8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b69789a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d82660d3f4cf4c7a81db",
+    "url": "/static/js/main.09ef667d.chunk.js"
+  },
+  {
+    "revision": "9228be4eca1d9fd9ac59",
+    "url": "/static/js/runtime-main.87619bf7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
